5cce99f51f045d5e71c8824c353746ce47cb22dd
pablopalomo3 <pablopalomo3@gmail.com>
2018-06-28 03:15:32 +0000

    assignment 12: complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   password_digest_nil = -> { puts "password_digest.nil?" }
   
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
   
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   
@@ -16,4 +17,13 @@ class User < ApplicationRecord
   
   has_secure_password
   
+  def format_name
+    if name
+      name_array = []
+      name.split.each do |name_part|
+        name_array << name_part.capitalize
+      end
+      self.name = name_array.join(" ")
+    end
+  end
 end
