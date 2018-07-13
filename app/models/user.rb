3e680746cedefcdd17d50916a0c627855eed2d78
pablopalomo3 <pablopalomo3@gmail.com>
2018-07-13 05:41:17 +0000

    checkpoint 17: complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   
   #used lambda to pass string to correct argument error
   password_digest_nil = -> { puts "password_digest.nil?" }
